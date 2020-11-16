ea415c1fbd997263916f544751a10c6124179ccc
Bastien Duban <duban.bastien@gmail.com>
2020-11-16 18:32:03 +0100

    controllers/views/devise

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,31 @@
+class BookingsController < ApplicationController
+before_action :set_pokemon, only: [:show, :destroy]
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to @booking, notice: 'You successfully booked a pokemon!'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @booking.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :total_price)
+  end
+end
