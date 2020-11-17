b3c8f31e2a88cf82589d8d1df4eaa8a2facf374a
gael gendre <gael.gendre@gmail.com>
2020-11-17 17:38:59 +0100

    bad style

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -2,7 +2,7 @@ class Pokemon < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings
-  # has_many_attached :photos
+  has_one_attached :photo
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true
   validates :abilities, presence: true
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,6 +2,16 @@ const { environment } = require('@rails/webpacker')
 
 const webpack = require('webpack');
 
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+  })
+)
+
+module.exports = environment
+
 // Preventing Babel from transpiling NodeModules packages
 environment.loaders.delete('nodeModules');
 
