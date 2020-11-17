18829465aa313cbfad97ceac2fc65653a46634f2
gael gendre <gael.gendre@gmail.com>
2020-11-17 15:31:36 +0100

    avec la seed

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,9 +1,9 @@
 class Pokemon < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings
-  has_many_attached :photos
+  # has_many_attached :photos
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true
-  validates :abilities, presence: true 
+  validates :abilities, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Pokemon.destroy_all
+
+sacha = User.create(username: "Sacha", email: "sacha@pokebnb.com", password: "123456")
+ondine = User.create(username: "Ondine", email: "ondine@pokebnb.com", password: "123456")
+
+dracaufeu = Pokemon.new(name: "Dracaufeu", price: 1050, abilities: "fire", level: 100)
+dracaufeu.user = sacha
+dracaufeu.save
+salameche = Pokemon.new(name: "Salamèche", price: 100, abilities: "fire", level: 15)
+salameche.user = sacha
+salameche.save
+pikachu = Pokemon.new(name: "Pikachu", price: 150, abilities: "electricity", level: 25)
+pikachu.user = sacha
+pikachu.save
+tortank = Pokemon.new(name: "Tortank", price: 2020, abilities: "water", level: 99)
+tortank.user = sacha
+tortank.save
+rondoudou = Pokemon.new(name: "Rondoudou", price: 80, abilities: "fairy", level: 12)
+rondoudou.user = ondine
+rondoudou.save
+ponyta = Pokemon.new(name: "Ponyta", price: 120, abilities: "fire", level: 19)
+ponyta.user = ondine
+ponyta.save
+evoli = Pokemon.new(name: "Evoli", price: 40, abilities: "normal", level: 8)
+evoli.user = ondine
+evoli.save
+florizarre = Pokemon.new(name: "Florizarre", price: 1050, abilities: "plant", level: 99)
+florizarre.user = ondine
+florizarre.save
