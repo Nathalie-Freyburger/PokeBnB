5d0b2eabcce54d3ffa18a940539e02d62e57d272
gael gendre <gael.gendre@gmail.com>
2020-11-17 14:12:10 +0100

    liens home, index, show

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :pokemons
+    resources :pokemons, only: [:index, :new, :create, :show, :destroy ] do
+      resources :bookings, only: [:new, :create, :destroy ]
+    end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
